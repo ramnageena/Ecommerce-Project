e4e545fe52f32764995da427bd0e3261fd6d08bf
Ram Tiwari <ramtiwari8716@gmail.com>
2024-06-19 01:20:18 +0530

    change for TRADE-4 commit# Refactor the Controller for Category

--- Ecommerce/src/main/java/net/ecommerce/EcommerceApplication.java
+++ Ecommerce/src/main/java/net/ecommerce/EcommerceApplication.java
@@ -1,16 +1,14 @@
 package net.ecommerce;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
-        System.out.println("Ecommerce Project is Started!!!");
+         System.out.println("Ecommerce Project is Started!!!");
 
     }
 
