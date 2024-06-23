36433e698263be12523adbf5e1137d11887435b8
Ram Tiwari <ramtiwari8716@gmail.com>
2024-06-24 00:14:00 +0530

    change for TRADE-17 commit# Added Product functionality

--- Ecommerce/src/main/java/net/ecommerce/payload/ProductDto.java
+++ Ecommerce/src/main/java/net/ecommerce/payload/ProductDto.java
@@ -1,5 +1,7 @@
 package net.ecommerce.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDto {
+
     private Long productId;
+    @NotEmpty(message = "Product should not be empty")
+    @Size(min = 4, message = "Product name must contain at least 4 characters")
     private String productName;
     private String image;
+    @NotEmpty(message = "Product should not be empty")
+    @Size(min = 6, message = "Product description must contain at least 6 characters")
     private String description;
     private Integer quantity;
     private double price;
