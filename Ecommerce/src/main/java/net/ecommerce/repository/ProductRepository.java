5598ecabbb72d86770473865894f0756d92c2344
Ram Tiwari <ramtiwari8716@gmail.com>
2024-06-24 00:14:21 +0530

    change for TRADE-18 commit# Added Product functionality

--- Ecommerce/src/main/java/net/ecommerce/repository/ProductRepository.java
+++ Ecommerce/src/main/java/net/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package net.ecommerce.repository;
 
+import net.ecommerce.model.Category;
 import net.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
