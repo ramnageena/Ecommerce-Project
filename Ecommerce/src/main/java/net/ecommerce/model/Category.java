a65dd88f0dc3fdb9b07c41089947392e969e6a15
Ram Tiwari <ramtiwari8716@gmail.com>
2024-06-24 00:12:33 +0530

    change for TRADE-14 commit# Added Product functionality

--- Ecommerce/src/main/java/net/ecommerce/model/Category.java
+++ Ecommerce/src/main/java/net/ecommerce/model/Category.java
@@ -7,16 +7,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "category_tbl")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Category {
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    List<Product> products;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-
     @NotEmpty(message = "Category should not be empty")
     @Size(min = 4, message = "Category name must contain at least 4 characters")
     private String categoryName;
