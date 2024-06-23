bbfe652666912a1087cef37b0de9a0bd7a1bef1c
Ram Tiwari <ramtiwari8716@gmail.com>
2024-06-24 00:14:41 +0530

    change for TRADE-19 commit# Added Product functionality

--- Ecommerce/src/main/java/net/ecommerce/payload/ProductResponse.java
+++ Ecommerce/src/main/java/net/ecommerce/payload/ProductResponse.java
@@ -3,7 +3,6 @@ package net.ecommerce.payload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.ecommerce.model.Product;
 
 import java.util.List;
 
@@ -12,5 +11,5 @@ import java.util.List;
 @NoArgsConstructor
 public class ProductResponse {
 
-    List<Product> content;
+    List<ProductDto> content;
 }
